ddbec2dae271a7eed554516f1104114f6c05d3a7
smc369 <smc369@cornell.edu>
2014-08-02 13:47:22 -0400

    Made FAQ page
    changed to .php file

---
+++ public_html/faq.php
@@ -0,0 +1,92 @@
+<!doctype html>
+
+<html>
+	<head>
+  	<title>FAQ</title>
+    <link rel="stylesheet" href="./css/style.css">	
+	</head>
+	<body>
+		
+		<div id="header">
+			<?php include('header.php') ?>
+		</div>
+		
+		<div class="content">
+			<h2>FAQ</h2>			
+			<ul>
+				<li>When do we meet?
+				<ul>
+   					<li>Wednesdays 7:00pm, Upson B7 (regular meetings)</li>
+    				<li>Fridays 5:00pm, Olin 165 (movie showings)</li>
+    			</ul>
+    			</li>
+    			
+  				<li>What do I have to do to join?
+    			<ul>
+    				<li>Just show up to a meeting!</li>
+    			</ul>
+    			</li>
+    			
+ 				<li>I don't any previous animation experience. Is that okay?
+ 				<ul>
+    				<li>Yes! Our weekly lessons don't require any previous knowledge.</li>
+    				<li>We also have officers available at lessons who can get you caught up if needed.</li>
+    			</ul>
+    			</li>
+    			
+  				<li>I have previous animation experience. Is that okay too?
+  				<ul>	
+    				<li>Yes! We have resources and lessons accessible to members with more experience.</li>
+    			</ul>
+    			</li>
+    			
+    			
+  				<li>I'd like to be on both the art and tech crews. Can I do that?
+  				<ul>
+    				<li><a href="index.html">Home</a></li>
+    			</ul>
+    			</li>
+    			
+    			<li>What if I can't make every meeting?
+  				<ul>
+    				<li>That's fine, we have a lesson archive on our site so you don't have to
+    				fall behind from missing a lesson.</li>
+    			</ul>
+    			</li>
+    			
+    			<li>What if I can't art?
+  				<ul>
+    				<li>We have members who can help teach you the basics, and you can 
+    				alternatively join the tech crew, which doesn't require the ability to
+    				draw.</li>
+    			</ul>
+    			</li>
+  
+    			<li>What sort of resources does the club have access to? 
+  				<ul>
+    				<li>We have a budget provided by the SAFC every year which allows us to 
+    				buy lesson memberships, art books, and other useful art assets.</li>
+    			</ul>
+    			</li>
+   
+    			<li>Which animation software do we use? 
+  				<ul>
+    				<li>We primarily use Maya, but many of our members have used Blender in the 
+    				past. The lab computers we use also have access to Adobe CS6. </li>
+    			</ul>
+    			</li>
+ 
+ 				<li>I want to partner with you guys for a project. What should I do?
+  				<ul>
+    				<li>Send us an email at cornellanimation@gmail.com</li>
+    			</ul>
+    			</li>
+    		</ul>
+		</div>
+		
+		<div id="footer">
+			<p> &copy; Computer Animation Club 2014 </p> 
+		</div>
+		
+	</body>
+</html>
