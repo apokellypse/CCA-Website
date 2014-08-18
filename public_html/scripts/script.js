490f52bbb80017ea0199ea87cb8c1d126e193a21
Kelly Yu <euriiko@gmail.com>
2014-08-18 02:02:15 -0400

    reformatted tables and font/text, added expand/contract capabilities to FAQ section for better ease of use

--- public_html/scripts/script.js
+++ public_html/scripts/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-	console.log("hi");
+	console.log("Welcome to the Cornell Computer Animation Club website.\nAssuming you're interested in how it's put together, we're currently looking for a person to\nhelp the officers maintin the website and keep it updated. Let us know if you're interested.")
 	$('aside').css("height", $(document).height()-160);
+
+	$('article > ul > li').click(function() {
+		var ind = $(this).index() + 1;
+		console.log("the index of the question is " + ind);
+		if ($('ul > li:nth-child(' + ind + ') span').html() == "+") {
+			$('ul > li:nth-child(' + ind + ') > ul > li').css("display", "block");
+			$('ul > li:nth-child(' + ind + ') span').html("&ndash;");
+			console.log("question " + ind + " is expanding");
+		} else {
+			$('ul > li:nth-child(' + ind + ') > ul > li').css("display", "none");
+			$('ul > li:nth-child(' + ind + ') span').html("+");
+			console.log("question " + ind + " is contracting");
+		}
+
+	})
+
 });
